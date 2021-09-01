cac4e63d698c4ca8272f341db57bbf4d9ed59285
cokayoka <suvorov.ilya-8991@mail.ru>
2021-09-01 18:57:09 +0500

    Созданы массивы с данными

---
+++ script.js
@@ -0,0 +1,109 @@
+let organisations = [{
+        id: 1,
+        name: "Lada",
+    },
+    {
+        id: 2,
+        name: "Audi",
+    },
+    {
+        id: 3,
+        name: "Toyota",
+    },
+
+];
+
+let positions = [{
+        id: 10,
+        position: "Директор",
+    },
+    {
+        id: 20,
+        position: "Инженер",
+    },
+    {
+        id: 30,
+        position: "Менеджер",
+    },
+]
+
+let employees = [{
+        id: 1,
+        fullName: "Сидоров Иван Петрович",
+        organisationId: 1,
+        positionId: 10,
+    },
+    {
+        id: 2,
+        fullName: "Клюквина Анастасия Викторовна",
+        organisationId: 1,
+        positionId: 30,
+    },
+    {
+        id: 3,
+        fullName: "Yoshimoro Katsumi",
+        organisationId: 3,
+        positionId: 10,
+    },
+    {
+        id: 4,
+        fullName: "Albrecht Wallenstein",
+        organisationId: 2,
+        positionId: 20,
+    },
+    {
+        id: 5,
+        fullName: "Архипов Федот Ярополкович",
+        organisationId: 1,
+        positionId: 20,
+    },
+    {
+        id: 6,
+        fullName: "Синицына Ксения Игоревна",
+        organisationId: 1,
+        positionId: 30,
+    },
+    {
+        id: 7,
+        fullName: "Gustaf Grefberg",
+        organisationId: 2,
+        positionId: 10,
+    },
+    {
+        id: 8,
+        fullName: "Simidzu Koyama",
+        organisationId: 3,
+        positionId: 20,
+    },
+    {
+        id: 9,
+        fullName: "Miura Hirana",
+        organisationId: 3,
+        positionId: 20,
+    },
+    {
+        id: 10,
+        fullName: "Кузьмин Егор Владимирович",
+        organisationId: 1,
+        positionId: 30,
+    },
+    {
+        id: 11,
+        fullName: "Мазурик Алёна Васильевна",
+        organisationId: 1,
+        positionId: 20,
+    },
+    {
+        id: 12,
+        fullName: "Gudrun Ensslin",
+        organisationId: 2,
+        positionId: 30,
+    },
+    {
+        id: 13,
+        fullName: "Ernst Rommel",
+        organisationId: 2,
+        positionId: 20,
+    },
+
+];
